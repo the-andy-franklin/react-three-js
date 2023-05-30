3e2755ccedca04213fd01c3d6f18514471859e5a
theandrewfranklin <theandrewfranklin@gmail.com>
2023-05-30 10:30:16 -0700

    cleaner

--- src/components/MyTorus.tsx
+++ src/components/MyTorus.tsx
@@ -6,22 +6,9 @@ import { useFrame } from 'react-three-fiber';
 const MyTorus = () => {
   const meshRef = useRef<Mesh>(null);
   const scroll = useScroll();
-  let previousOffset = 0; // I'm intentionally not using useState here
   
   useFrame(() => {
-    if (meshRef.current) {
-      if (scroll.delta > 0) {
-        if (scroll.offset > previousOffset) meshRef.current.rotateOnAxis(new Vector3(1, 1, 0), scroll.delta * 6.25)
-        if (scroll.offset < previousOffset) meshRef.current.rotateOnAxis(new Vector3(1, 1, 0), scroll.delta * -6.25)
-        previousOffset = scroll.offset;
-      }
-
-      if (scroll.delta === 0) {
-        if (meshRef.current.rotation.x !== 0) meshRef.current.rotateX(-meshRef.current.rotation.x / 24)
-        if (meshRef.current.rotation.y !== 0) meshRef.current.rotateY(-meshRef.current.rotation.y / 24)
-        if (meshRef.current.rotation.z !== 0) meshRef.current.rotateZ(-meshRef.current.rotation.z / 24)
-      }
-    }
+    meshRef.current?.setRotationFromAxisAngle(new Vector3(1, 1, 0), scroll.offset * 6.25)
   });
 
   return (
